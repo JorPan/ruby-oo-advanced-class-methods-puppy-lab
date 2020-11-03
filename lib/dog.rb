c39d58c12f1236ca30cbbcd8901e163b5b344744
JorPan <jbpanasewicz@gmail.com>
2020-11-02 18:28:15 -0700

    commit tree 6dbeb0727f10866ad89a16dddcf1570f8cc64d5c

--- lib/dog.rb
+++ lib/dog.rb
@@ -1,1 +1,48 @@
-# Add your code here
+require "pry"
+
+
+
+class Dog
+
+    attr_accessor :name
+
+    @@all = []
+
+    def initialize name
+        @name = name
+
+        save
+    end
+
+    def self.all
+        @@all
+    end
+    
+    def self.clear_all
+        @@all.clear
+    end
+
+    def save
+        @@all << self
+    end
+    
+pluto = Dog.new("Pluto")
+fido = Dog.new("Fido")
+maddy = Dog.new("Maddy")
+
+    def self.print_all
+        @@all.map do |dog|
+            dog.name
+        end
+    end
+
+
+
+end
+
+
+
+
+
+# binding.pry 
+# 0
